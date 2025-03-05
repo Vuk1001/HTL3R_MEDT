bc0774ba1d39081556a0731b1221ffd0fc0cd50a
Vuk1001 <vukvs3a@gmail.com>
2025-03-05 14:33:11 +0100

    Fertig MVC

---
+++ Views_in_MVC/index.php
@@ -0,0 +1,61 @@
+<?php
+$templateFile = 'template.html';
+if (!file_exists($templateFile)) {
+    die("Template file not found.");
+}
+
+$fp = fopen($templateFile, 'r');
+$templateContent = fread($fp, filesize($templateFile));
+fclose($fp);
+
+$hotels = [
+    [
+        'hotel_name' => 'HTL Rennweg',
+        'hotel_description' => 'A little uncomfortable but warm.',
+        'hotel_rating' => '2.5 Stars'
+    ],
+    [
+        'hotel_name' => 'Reumannplatz',
+        'hotel_description' => 'Scary, wouldnt recommend',
+        'hotel_rating' => '1 Star'
+    ],
+    [
+        'hotel_name' => 'McDonalds Stephansplatz',
+        'hotel_description' => 'Tasty but mean. They kicked me out.',
+        'hotel_rating' => '3 Stars'
+    ]
+    ,
+    [
+        'hotel_name' => 'Sigma Nation Gaming',
+        'hotel_description' => 'English video talk for 10 minutes.',
+        'hotel_rating' => '5 Stars'
+    ]
+
+];
+
+$loopStartTag = '<!-- START_HOTEL_LOOP -->';
+$loopEndTag   = '<!-- END_HOTEL_LOOP -->';
+
+$startPos = strpos($templateContent, $loopStartTag);
+$endPos   = strpos($templateContent, $loopEndTag);
+
+if ($startPos !== false && $endPos !== false) {
+    $startPos += strlen($loopStartTag);
+    $loopBlock = substr($templateContent, $startPos, $endPos - $startPos);
+
+    $hotelOutput = '';
+
+    foreach ($hotels as $hotel) {
+        $currentBlock = $loopBlock;
+        foreach ($hotel as $key => $value) {
+            $currentBlock = str_replace('{{' . $key . '}}', $value, $currentBlock);
+        }
+        $hotelOutput .= $currentBlock;
+    }
+
+    $templateContent = substr_replace($templateContent, $hotelOutput, $startPos, $endPos - $startPos);
+    $templateContent = str_replace([$loopStartTag, $loopEndTag], '', $templateContent);
+}
+
+echo $templateContent;
+?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '2bb9c147e067f01a740aca43f0b9d7ab88253756',
+        'reference' => '7fea424dd14591ff3072ec6b4250670676b7454b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '2bb9c147e067f01a740aca43f0b9d7ab88253756',
+            'reference' => '7fea424dd14591ff3072ec6b4250670676b7454b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
