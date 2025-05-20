e5b260e5a03d71225b202d629b9b537a2562d4d4
Niqkl <1201@htl.rennweg.at>
2025-05-20 16:54:36 +0200

    SASS für MVC

--- DoctrineDBAL/public/save_round.php
+++ DoctrineDBAL/public/save_round.php
@@ -6,19 +6,40 @@ $playedAt = date('Y-m-d H:i:s');
 
 $db = Connection::getInstance();
 
-$db->insert('game_rounds', [
-    'round_number' => $_POST['round_number'],
-    'player_name' => $_POST['player1_name'],
-    'player_symbol' => $_POST['player1_symbol'],
-    'played_at' => $playedAt
-]);
+$roundNumber = $_POST['round_number'];
 
-$db->insert('game_rounds', [
-    'round_number' => $_POST['round_number'],
-    'player_name' => $_POST['player2_name'],
-    'player_symbol' => $_POST['player2_symbol'],
-    'played_at' => $playedAt
-]);
+$db->createQueryBuilder()
+    ->insert('game_rounds')
+    ->values([
+        'round_number' => ':round_number',
+        'player_name' => ':player1_name',
+        'player_symbol' => ':player1_symbol',
+        'played_at' => ':played_at'
+    ])
+    ->setParameters([
+        'round_number' => $roundNumber,
+        'player1_name' => $_POST['player1_name'],
+        'player1_symbol' => $_POST['player1_symbol'],
+        'played_at' => $playedAt
+    ])
+    ->executeStatement();
+
+
+$db->createQueryBuilder()
+    ->insert('game_rounds')
+    ->values([
+        'round_number' => ':round_number',
+        'player_name' => ':player2_name',
+        'player_symbol' => ':player2_symbol',
+        'played_at' => ':played_at'
+    ])
+    ->setParameters([
+        'round_number' => $roundNumber,
+        'player2_name' => $_POST['player2_name'],
+        'player2_symbol' => $_POST['player2_symbol'],
+        'played_at' => $playedAt
+    ])
+    ->executeStatement();
 
 header('Location: /');
 exit();
